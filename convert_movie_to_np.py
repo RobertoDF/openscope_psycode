76bd6bbf72958381ceb73fd91606fc2a3467816c
David Turner <davidt0x@gmail.com>
2023-04-20 18:25:20 -0400

    Create a gray blank screen video.
    There is probably a way to generate this using camstim. For now we will do this.

--- convert_movie_to_np.py
+++ convert_movie_to_np.py
@@ -5,6 +5,11 @@ import numpy as np
 
 vid = skvideo.io.vread("data/Movie_sequence_6s.mp4")
 
-np.save('data/movie_clip_A.npy', vid[0:120, :, :, 0])
-np.save('data/movie_clip_B.npy', vid[121:241, :, :, 0])
-np.save('data/movie_clip_C.npy', vid[240:360, :, :, 0])
+vid_a = vid[0:120, :, :, 0]
+vid_b = vid[121:241, :, :, 0]
+vid_c = vid[240:360, :, :, 0]
+
+np.save('data/movie_clip_A.npy', vid_a)
+np.save('data/movie_clip_B.npy', vid_b)
+np.save('data/movie_clip_C.npy', vid_c)
+np.save('data/gray.npy', np.ones_like(vid_a)*128)
