3e15cc2ae715809365549b1f7ea95270e4105c37
David Turner <davidt0x@gmail.com>
2023-04-21 13:06:43 -0400

    Setup monitor like example experiment.

--- stimulus.py
+++ stimulus.py
@@ -1,12 +1,22 @@
 import numpy as np
 
-from camstim import MovieStim, SweepStim, Window
+from psychopy import monitors
+from camstim import MovieStim, SweepStim, Window, Warp
+
 import logging
 
+# Make logging a bit prettier
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# movie_clip_files = ['data/movie_clip_A.npy', 'data/movie_clip_B.npy', 'data/movie_clip_C.npy', 'data/gray.npy']
+# Change depending on the DAY of the experiment. Maybe this should be a command line argument?
+DAY = 0
+
+# Paths to the movie clip files to load.
+movie_clip_files = ['data/movie_clip_A.npy', 'data/movie_clip_B.npy', 'data/movie_clip_C.npy', 'data/gray.npy']
+
+# Set of debug movie clips to load. These videos have blinking letters to make clips easier to recognize.
+# If they don't exist yet make sure to run gen_letter_videos.py first. Comment out if not debugging!
 movie_clip_files = ['data/A_blinking_video.npy', 'data/B_blinking_video.npy', 'data/C_blinking_video.npy',
                     'data/gray.npy']
 
@@ -20,9 +30,9 @@ order70 = (np.loadtxt('data/stimulus_orderings/movie_order_random_70min.txt').as
 def make_movie_stimulus(movie_paths, order, window):
     """Generate a Stimulus that plays a series of movie clips in a specified order."""
 
-    # Convert the order into a list of display sequence tuples. There should be one display sequence list per movie clip.
-    # Each display sequence list contains a set of tuples of the form (start_second, stop_second). Theses tuples define
-    # the start and stop time for when to play the clip and are determined by the order vector
+    # Convert the order into a list of display sequence tuples. There should be one display sequence list per movie
+    # clip. Each display sequence list contains a set of tuples of the form (start_second, stop_second). Theses tuples
+    # define the start and stop time for when to play the clip and are determined by the order vector
     all_starts = np.arange(0, 2 * len(order), 2).astype(float)
     display_sequences = []
     for i in np.unique(order):
@@ -58,21 +68,32 @@ def make_movie_stimulus(movie_paths, order, window):
     return stim
 
 
-window = Window(fullscr=False,
-                monitor='testMonitor',
-                screen=1, )
+if __name__ == "__main__":
 
-config = {
-    'sync_sqr': True,
-}
+    # Copied monitor and window setup from:
+    # https://github.com/AllenInstitute/openscope-glo-stim/blob/main/test-scripts/cohort-1-test-12min-drifting.py
 
-day = 0  # change the value of day for different days
+    dist = 15.0
+    wid = 52.0
 
-if day == 0 or day == 5:
-    order = order20
-else:
-    order = order50
+    monitor = monitors.Monitor("testMonitor", distance=dist, width=wid) #"Gamma1.Luminance50"
 
-ss = make_movie_stimulus(movie_clip_files, order, window)
+    # Create display window
+    window = Window(fullscr=True, # Will return an error due to default size. Ignore.
+                    monitor=monitor,  # Will be set to a gamma calibrated profile by MPE
+                    screen=0,
+                    warp=Warp.Spherical
+                    )
 
-ss.run()
+    config = {
+        'sync_sqr': True,
+    }
+
+    if DAY == 0 or DAY == 5:
+        order = order20
+    else:
+        order = order50
+
+    ss = make_movie_stimulus(movie_clip_files, order, window)
+
+    ss.run()
